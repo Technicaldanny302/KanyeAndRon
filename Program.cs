9c455892a384305b1dffd61207dff26957716fd2
Dan Marino <technicaldanny302@gmail.com>
2024-09-14 20:34:01 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+
+
+namespace KanyeAndRon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+        }
+    }
+}
